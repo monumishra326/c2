e8ce221e46c32b2a2e9f236e78523245863c2117
Monu MIshra <96140349+monumishra326@users.noreply.github.com>
2022-03-14 12:47:48 +0530

    Update Rentals.jsx

--- src/Components/Rentals/Rentals.jsx
+++ src/Components/Rentals/Rentals.jsx
@@ -1,14 +1,89 @@
 import "./Rentals.css";
+import axios from "axios";
+
+import { useState, useEffect } from "react";
 
 export const Rentals = () => {
+  const [getdata, setgetdata] = useState([]);
+
+  const details = () => {
+    axios.get("http://localhost:8080/houses").then((res) => {
+      var alldata = res.data;
+      setgetdata(alldata);
+    });
+  };
+
+  const asc = () => {
+    axios.get("http://localhost:8080/houses").then((res) => {
+      var alldata = res.data;
+      var ascsort = alldata.sort((a, b) => {
+        return a.id - b.id;
+      });
+      setgetdata(ascsort);
+    });
+  };
+  const rentasc = () => {
+    axios.get("http://localhost:8080/houses").then((res) => {
+      var alldata = res.data;
+      var ascsort = alldata.sort((a, b) => {
+        return a.rent - b.rent;
+      });
+      setgetdata(ascsort);
+    });
+  };
+  const rentdsc = () => {
+    axios.get("http://localhost:8080/houses").then((res) => {
+      var alldata = res.data;
+      var ascsort = alldata.sort((a, b) => {
+        return b.rent - a.rent;
+      });
+      setgetdata(ascsort);
+    });
+  };
+
+  const areadsc = () => {
+    axios.get("http://localhost:8080/houses").then((res) => {
+      var alldata = res.data;
+      var ascsort = alldata.sort((a, b) => {
+        return b.areaCode - a.areaCode;
+      });
+      setgetdata(ascsort);
+    });
+  };
+  const areaasc = () => {
+    axios.get("http://localhost:8080/houses").then((res) => {
+      var alldata = res.data;
+      var ascsort = alldata.sort((a, b) => {
+        return a.areaCode - b.areaCode;
+      });
+      setgetdata(ascsort);
+    });
+  };
+
+  useEffect(() => {
+    details();
+  }, []);
+
+  //   var pass = details();
+
   return (
     <div className="rentalContainer">
       <div className="sortingButtons">
-        <button className="sortById">Sort by ID</button>
-        <button className="sortByRentAsc">Rent Low to high</button>
-        <button className="sortByRentDesc">Rent High to low</button>
-        <button className="sortByAreaAsc">Area Low to high</button>
-        <button className="sortByAreaDesc">Area High to Low</button>
+        <button className="sortById" onClick={asc}>
+          Sort by ID
+        </button>
+        <button className="sortByRentAsc" onClick={rentasc}>
+          Rent Low to high
+        </button>
+        <button className="sortByRentDesc" onClick={rentdsc}>
+          Rent High to low
+        </button>
+        <button className="sortByAreaAsc" onClick={areaasc}>
+          Area Low to high
+        </button>
+        <button className="sortByAreaDesc" onClick={areadsc}>
+          Area High to Low
+        </button>
       </div>
       <input
         className="searchAddress"
@@ -29,7 +104,7 @@ export const Rentals = () => {
           </tr>
         </thead>
         <tbody>
-          {[].map((house, index) => {
+          {getdata.map((house, index) => {
             return (
               <tr key={house.id} className="houseDetails">
                 <td className="houseId">{house.id}</td>
