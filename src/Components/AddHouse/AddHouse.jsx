bc21e140019dcaabed626e555a4aa4c6ab6fecf6
Monu MIshra <96140349+monumishra326@users.noreply.github.com>
2022-03-14 12:45:47 +0530

    Update AddHouse.jsx

--- src/Components/AddHouse/AddHouse.jsx
+++ src/Components/AddHouse/AddHouse.jsx
@@ -1,60 +1,120 @@
-import React, {useState,useEffect} from "react";
-import axios  from "axios";
-
-
-
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 export const AddHouse = () => {
-//   const[userdata,setUserdata]=useState({
-//     name:"",
-//     ownerName:"",
-//     addres:"",
-//     areaCode:"",
-//     rent:"",
-//     preferredTenant:"",
-//     image:"",
+  const [houses, sethouses] = useState({
+    name: "",
+    ownerName: "",
+    address: "",
+    areaCode: "",
+    rent: "",
+    bachelor: "",
+    married: "",
+    image: "",
+  });
 
-//   });
-//   const[getdata,setgetdata]=useState([]);
-//   const handleinput=(e)=>{
-//     setUserdata({...userdata,[id]: value});
+ 
 
-//   };
-//   const handlesubmit=(e)=>{
-//     e.preventDefault();
-  
-//   }
-    return (
-      <div className="addHouseContainer">
-        <form>
-          <label>name</label>
-          <input type="text" className="name" required />
-          <br />
-          <label>ownerName</label>
-          <input  type="text" className="ownerName" required />
-          <br />
-          <label>address</label>
-          <input  type="text" className="address" required />
-          <br />
-          <label>areaCode</label>
-          <input  type="text" className="areaCode" required />
-          <br />
-          <label>rent</label>
-          <input  type="text" className="rent" required />
-          <br />
-          <label>preferredTenant</label>
-          <br />
-          <label>bachelor</label>
-          <input  type="checkbox" className="bachelor" />
-          <br />
-          <label>married</label>
-          <input  type="checkbox" className="married" />
-          <br />
-          <label>image</label>
-          <input  type="text" className="image" required />
-          <br />
-          <input className="submitBtn" type="submit" />
-        </form>
-      </div>
-    );
+  const InputHandler = (e) => {
+    const { id, value } = e.target;
+    sethouses({ ...houses, [id]: value });
   };
+
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    axios.post("http://localhost:8080/houses", houses).then(() => {
+      alert("data submitted");
+
+     
+    });
+  };
+  
+
+  return (
+    <div className="addHouseContainer">
+      <form>
+        <label>name</label>
+        <input
+          type="text"
+          className="name"
+          id="name"
+          //   value={input}
+          required
+          onChange={InputHandler}
+        />
+        <br />
+        <label>ownerName</label>
+        <input
+          //   value={input}
+          type="text"
+          className="ownerName"
+          id="ownerName"
+          required
+          onChange={InputHandler}
+        />
+        <br />
+        <label>address</label>
+        <input
+          //   value={input}
+          type="text"
+          className="address"
+          id="address"
+          required
+          onChange={InputHandler}
+        />
+        <br />
+        <label>areaCode</label>
+        <input
+          //   value={input}
+          type="text"
+          className="areaCode"
+          id="areaCode"
+          required
+          onChange={InputHandler}
+        />
+        <br />
+        <label>rent</label>
+        <input
+          //   value={input}
+          type="text"
+          className="rent"
+          id="rent"
+          required
+          onChange={InputHandler}
+        />
+        <br />
+        <label>preferredTenant</label>
+        <br />
+        <label>bachelor</label>
+        <input
+          value="bachelor"
+          type="checkbox"
+          className="bachelor"
+          id="bachelor"
+          onChange={InputHandler}
+        />
+        <br />
+        <label>married</label>
+        <input
+          value="married"
+          type="checkbox"
+          className="married"
+          id="married"
+          onChange={InputHandler}
+        />
+        <br />
+        <label>image</label>
+        <input
+          //   value={""}
+          type="text"
+          className="image"
+          id="image"
+          required
+          onChange={InputHandler}
+        />
+        <br />
+        <input className="submitBtn" type="submit" onClick={handlesubmit} />
+      </form>
+    </div>
+  );
+};
